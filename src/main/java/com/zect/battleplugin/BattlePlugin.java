4def30f8eebef81977558684b12ec6b2e3d78ce0
Zect <57027411+sas08@users.noreply.github.com>
2021-05-24 21:30:25 +0900

    [Make]: 適当

---
+++ src/main/java/com/zect/battleplugin/BattlePlugin.java
@@ -0,0 +1,28 @@
+package com.zect.battleplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BattlePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    // onCommand は plugin.yml に記載されたコマンドが呼ばれた時に実行
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        sender.sendMessage("ハロー");
+        sender.sendMessage(cmd.getName());
+        return true;
+    }
+}
