117f91a59afe8cf84ea8dbdd9b107ffeba4c45eb
Zect <57027411+sas08@users.noreply.github.com>
2021-05-24 22:17:35 +0900

    [Add]: サブコマンド処理

--- src/main/java/com/zect/battleplugin/BattlePlugin.java
+++ src/main/java/com/zect/battleplugin/BattlePlugin.java
@@ -1,5 +1,6 @@
 package com.zect.battleplugin;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -21,8 +22,37 @@ public final class BattlePlugin extends JavaPlugin {
     // onCommand は plugin.yml に記載されたコマンドが呼ばれた時に実行
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        sender.sendMessage("ハロー");
-        sender.sendMessage(cmd.getName());
+        // argsが無かったら、切り返す
+        if (args.length == 0) {
+            return false;
+        }
+        String output;
+        switch (args[0]) {
+            case "start":
+                output = "攻城戦を開始します";
+                break;
+            case "check":
+                output = "設定を表示します";
+                break;
+            case "setfightteam":
+                output = "参加チームに追加";
+                break;
+            case "setwatchteam":
+                output = "観覧チームに追加";
+                break;
+            case "setborder":
+                output = "範囲指定";
+                break;
+            case "setspawn":
+                output = "スポーン地点設定";
+                break;
+            case "settimelimit":
+                output = "制限自邸を設定";
+                break;
+            default:
+                return false;
+        }
+        sender.sendMessage(output);
         return true;
     }
 }
