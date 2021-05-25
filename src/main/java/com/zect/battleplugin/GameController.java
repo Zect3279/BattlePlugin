7e96037c295ff4063ee30eb992fbccda458a82ec
Zect <57027411+sas08@users.noreply.github.com>
2021-05-25 10:30:19 +0900

    Update GameController.java

--- src/main/java/com/zect/battleplugin/GameController.java
+++ src/main/java/com/zect/battleplugin/GameController.java
@@ -2,8 +2,21 @@ package com.zect.battleplugin;
 
 public class GameController {
 
-  static void start() {
-  // ゲーム開始機構
-  }
+    static void start() {
+        // ゲーム開始機構
+        
+        /* titleでカウントダウン
+        * 『ゲーム開始まで 5秒前』
+        * 『3』
+        * 『2』
+        * 『1』
+        * 『ゲーム開始』
+        */
+        
+        /*
+        * ボーダー処理・殺害処理・勝敗判定
+        * の関数を発火
+        */
+    }
 
 }
