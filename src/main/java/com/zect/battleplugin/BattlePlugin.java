706beced8df1808a089d857d7898447a9276eff5
Zect <57027411+sas08@users.noreply.github.com>
2021-05-25 10:30:17 +0900

    Update BattlePlugin.java

--- src/main/java/com/zect/battleplugin/BattlePlugin.java
+++ src/main/java/com/zect/battleplugin/BattlePlugin.java
@@ -35,16 +35,18 @@ public final class BattlePlugin extends JavaPlugin {
                 CheckSettings(sender,cmd,args);
                 break;
             case "setfightteam":
-                output = "参加チームに追加";
+                // output = "参加チームに追加";
+                AddFighters(sender,cmd,args);
                 break;
             case "setwatchteam":
-                output = "観覧チームに追加";
-                break;
-            case "setborder":
-                output = "範囲指定";
+                // output = "観覧チームに追加";
+                SetWatcher(sender,cmd,args);
                 break;
             case "setspawn":
                 output = "スポーン地点設定";
+                break;
+            case "setborder":
+                output = "範囲指定";
                 break;
             case "settimelimit":
                 output = "制限自邸を設定";
@@ -56,22 +58,58 @@ public final class BattlePlugin extends JavaPlugin {
         return true;
     }
     public void GameStart(CommandSender sender, Command cms, String[] args) {
+        // ゲーム開始
         GameController.start();
     }
+    public void AddFighters(CommandSender sender, Command cmd, String[] args) {
+        // 戦闘チーム追加
+        // 既に２つあったら、古いやつを消す
+    }
+    public void SetWatcher(CommandSender sender, Command cmd, String[] args) {
+        // 観覧チームを指定
+        // 一つのみ指定可能
+    }
     public void CheckSettings(CommandSender sender, Command cmd, String[] args) {
+        // 設定一覧を表示
+        // チーム1のリス地へTP
+        // チーム2のリス地へTP
+        // ゲームスタートボタン
         BaseComponent[] check = new ComponentBuilder(
-                new TextComponent(new ComponentBuilder()
-                        .append("[攻城戦支援プラグイン] ").color(ChatColor.LIGHT_PURPLE)
-                        .append("設定一覧を表示予定").color(ChatColor.GREEN)
-                        .append("[クリックで対戦を開始]").color(ChatColor.GOLD)
-                        .create())
-        )
-                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder()
+            new TextComponent(new ComponentBuilder()
+                .append("[攻城戦支援プラグイン] ").color(ChatColor.LIGHT_PURPLE)
+                .append("設定一覧を表示予定").color(ChatColor.GREEN)
+                .append(
+                    new ComponentBuilder("[Team1リス地へTP]")
+                    .color(ChatColor.RED)
+                    .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder()
+                        .append("クリックでチーム1リス地へTP")
+                        .create()
+                    ))
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, ("/tp ") + ResTeam1))
+                    .create()
+                )
+                .append(
+                    new ComponentBuilder("[Team2リス地へTP]")
+                    .color(ChatColor.BLUE)
+                    .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder()
+                        .append("クリックでチーム2リス地へTP")
+                        .create()
+                    ))
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, ("/tp ") + ResTeam2))
+                    .create()
+                )
+                .append(
+                    new ComponentBuilder("[クリックで対戦を開始]")
+                    .color(ChatColor.GOLD)
+                    .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder()
                         .append("クリックで攻城戦を開始")
                         .create()
-                ))
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, ("/battle start") + token))
-                .create();
+                    ))
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/battle start"))
+                    .create()
+                ).create()
+             )
+        );
         sender.sendMessage(check);
     }
     
