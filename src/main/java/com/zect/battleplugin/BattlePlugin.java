bdb986be37ddd296e7b3a1458c15fffde41c10be
Zect <57027411+sas08@users.noreply.github.com>
2021-05-26 12:30:09 +0900

    [Add]: checkの条件分岐

--- src/main/java/com/zect/battleplugin/BattlePlugin.java
+++ src/main/java/com/zect/battleplugin/BattlePlugin.java
@@ -13,8 +13,8 @@ import java.util.Optional;
 
 public final class BattlePlugin extends JavaPlugin {
 
-    private String ResTeam1;
-    private String ResTeam2;
+    private Location ResTeam1;
+    private Location ResTeam2;
 
     @Override
     public void onLoad() {
@@ -22,18 +22,25 @@ public final class BattlePlugin extends JavaPlugin {
         new CommandAPICommand("battle")
         .withArguments(new StringArgument("start"))
         .executes((sender, args) -> {
+            // `/battle start`を実行したらゲームを開始する
             GameStart(sender,args);
         })
         .withArguments(new StringArgument("check"))
+        .executes((sender, args) -> {
+            // `/battle check`を実行したら設定一覧を表示する
+            CheckSettings(sender, args);
+        })
         .withSubcommand(new CommandAPICommand("FightTeam")
             .withSubCommand(new CommandAPICommand("add"))
                 .withArguments(new TeamArgument("team"))
                 .executes((sender, args) -> {
+                    // `/battle FightTeam add [team]`を実行したら参加チームに追加する
                     AddFighters(sender,args);
                 })
             .withSubCommand(new CommandAPICommand("remove"))
                 .withArguments(new TeamArgument("team"))
                 .executes((sender, args) -> {
+                    // `/battle remove [team]`を実行したら参加チームから削除する
                     RemoveFighters(sender,args);
                 })
         )
@@ -41,11 +48,13 @@ public final class BattlePlugin extends JavaPlugin {
             .withSubCommand(new CommandAPICommand("add"))
                 .withArguments(new TeamArgument("team"))
                 .executes((sender, args) -> {
+                    // `/battle WatcherTeam add [team]`を実行したら観覧チームに追加する
                     AddWatcher(sender,args);
                 })
             .withSubCommand(new CommandAPICommand("remove"))
                 .withArguments(new TeamArgument("team"))
                 .executes((sender, args) -> {
+                    // `/battle WatcherTeam remove [team]`を実行したら観覧チームから削除する
                     RemoveWatcher(sender,args);
                 })
         )
@@ -81,7 +90,11 @@ public final class BattlePlugin extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
-
+    
+    public void CheckCanPlay() {
+        // ゲーム開始可能か確認する
+    }
+    
     public void GameStart(CommandSender sender, String[] args) {
         // ゲーム開始
         GameController.start();
@@ -113,43 +126,56 @@ public final class BattlePlugin extends JavaPlugin {
                 .append("設定一覧を表示予定").color(ChatColor.GREEN)
                 .create())
         ).create();
-        BaseComponent[] TP1 = new ComponentBuilder(
+        if (ResTeam1 != null) {
+            BaseComponent[] TP1 = new ComponentBuilder(
                 new TextComponent(new ComponentBuilder()
                         .append("[Team1リス地へTP] ").color(ChatColor.GOLD)
                         .create())
-        )
+            )
                 .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder()
                     .append("クリックでチーム1のリス地へTP")
                     .create()
                 ))
                 .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, ("/tp ") + ResTeam1))
                 .create();
-        BaseComponent[] TP2 = new ComponentBuilder(
-                new TextComponent(new ComponentBuilder()
-                        .append("[Team2リス地へTP] ").color(ChatColor.GOLD)
-                        .create())
-        )
-                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder()
-                    .append("クリックでチーム2リス地へTP")
-                    .create()
-                ))
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, ("/tp ") + ResTeam2))
-                .create();
-        BaseComponent[] starting = new ComponentBuilder(
-                new TextComponent(new ComponentBuilder()
-                        .append("[クリックで対戦を開始] ").color(ChatColor.GOLD)
-                        .create())
-        )
-                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder()
-                    .append("クリックで攻城戦を開始")
-                    .create()
-                ))
-                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/battle start"))
-                .create();
+        }
+        if (ResTeam2 != null) {
+            BaseComponent[] TP2 = new ComponentBuilder(
+                    new TextComponent(new ComponentBuilder()
+                            .append("[Team2リス地へTP] ").color(ChatColor.GOLD)
+                            .create())
+            )
+                    .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder()
+                        .append("クリックでチーム2リス地へTP")
+                        .create()
+                    ))
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, ("/tp ") + ResTeam2))
+                    .create();
+        }
+        if (CheckCanPlay == true) {
+            BaseComponent[] starting = new ComponentBuilder(
+                    new TextComponent(new ComponentBuilder()
+                            .append("[クリックで対戦を開始] ").color(ChatColor.GOLD)
+                            .create())
+            )
+                    .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder()
+                        .append("クリックで攻城戦を開始")
+                        .create()
+                    ))
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/battle start"))
+                    .create();
+        }
+        
         sender.sendMessage(check);
-        sender.sendMessage(TP1);
-        sender.sendMessage(TP2);
-        sender.sendMessage(starting);
+        if (TP1 != null) {
+            sender.sendMessage(TP1);
+        }
+        if (TP2 != null) {
+            sender.sendMessage(TP2);
+        }
+        if (starting != null) {
+            sender.sendMessage(starting);
+        }
     }
     
 }
