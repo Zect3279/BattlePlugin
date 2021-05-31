95db7516a5499fc84d169e08c7b7786e09a840b4
Zect <57027411+sas08@users.noreply.github.com>
2021-06-01 08:35:30 +0900

    [Add]: 引数を受け取る / 使うであろう変数を定義

--- src/main/java/com/zect/battleplugin/GameController.java
+++ src/main/java/com/zect/battleplugin/GameController.java
@@ -1,8 +1,31 @@
 package com.zect.battleplugin;
 
-public class GameController {
+import org.bukkit.plugin.java.JavaPlugin;
 
-    static void start() {
+public class GameController extends JavaPlugin {
+
+    static void start(Server server, Scoreboard MainBoard, Map<String, String> TeamName, Location Corner, Map<String, Location> TeamRes, Integer timeLimit) {
+        /* 引数
+        * - [x] 戦闘チーム
+        * - [x] 観覧チーム
+        * - [x] リス地
+        * - [x] 範囲
+        * - [x] 制限時間
+        * - [x] スコアボードobj
+        * - [x] サーバーobj
+        */
+        
+        // 使うであろう変数を用意
+//         Server server = sender.getServer();
+//         Scoreboard MainBoard = server.getScoreboardManager().getMainScoreboard();
+        Team Team1 = MainBoard.getTeam(TeamName.get("Team1"));
+        Team Team2 = MainBoard.getTeam(TeamName.get("Team2"));
+        Team Team3 = MainBoard.getTeam(TeamName.get("Team3"));
+        Location Corner1 = Corner.get(0);
+        Location Corner2 = Corner.get(1);
+        Location Spawn1 = TeamRes.get("Team1");
+        Location Spawn2 = TeamRes.get("Team2");
+        
         // ゲーム開始機構
         
         /* titleでカウントダウン
