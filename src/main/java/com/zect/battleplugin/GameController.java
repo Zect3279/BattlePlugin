28475ee04149f2c4f782a537b7a2b51f1df66816
Zect <57027411+sas08@users.noreply.github.com>
2021-06-02 12:21:40 +0900

    [Add]: プレイヤー死亡処理
    - プレイヤーが死亡したときの処理をEventを使うようにした。
    - 攻城戦中に参加した参加勢をチームに所属させ、強制的にリス地にTPさせる。

--- src/main/java/com/zect/battleplugin/GameController.java
+++ src/main/java/com/zect/battleplugin/GameController.java
@@ -1,8 +1,11 @@
 package com.zect.battleplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
 
-public class GameController extends JavaPlugin {
+public class GameController extends JavaPlugin implements Listener {
+    
+    public boolean GamePlaying = false;
 
     public static void start(Server server, Scoreboard MainBoard, Map<String, String> TeamName, Location Corner, Map<String, Location> TeamRes, Integer timeLimit) {
         /* 引数
@@ -44,25 +47,50 @@ public class GameController extends JavaPlugin {
         * ボーダー処理・殺害処理・勝敗判定
         * の関数を発火
         */
+        GamePlaying = true;
         Integer num = 5;
         ExecutorService exec = Executors.newCachedThreadPool();
         while (num > 0) {
             exec.submit(new Bording());
-            exec.submit(new KillCount());
             exec.submit(new WinChecker());
         }
+    // プレイヤーが死んだらインスタントが呼び出される
+    @EventHandler
+    public void onPlayerDead (PlayerDeathEvent e) {
+        if (!GamePlaying) {
+            return;
+        }
+        if (e.getEntity() instanceof Player) {
+            // 死んだ人の処理
+        }
+
+        if (e.getEntity().getKiller() instanceof Player) {
+            // 殺した人の処理
+        }
+    }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // 攻城戦中に入ってくたら、攻城戦に強制参加
+        // チームに参加していなかったら、人数が少ない方のチームに入れる
+        // チームリス地に強制的にTPする
+        if (!GamePlaying) {
+            return;
+        }
+        /* 
+        * 『あなたは、[チーム名] に所属しました。』
+        * 『後5秒でリス地にTPします。』
+        * 『3』
+        * 『2』
+        * 『1』
+        * 『TP完了』
+        */
+    }
 }
 
 public class Bording implements Runnable {
 
     public void run() {
         // ボーダー処理
-    }
-}
-public class KillCount implements Runnable {
-
-    public void run() {
-        // 殺害判定
     }
 }
 public class WinChecker implements Runnable {
